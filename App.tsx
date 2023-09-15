bf99a4ae98aa769adfac5065727e2fdda1412a76
IlliaBezditnyi <GreyIl@protonmail.com>
2023-09-15 16:06:17 +0300

    redux provider for app file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import store from './src/store';
+import PopulationScreen from './src/screens/PopulationScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PopulationScreen />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
